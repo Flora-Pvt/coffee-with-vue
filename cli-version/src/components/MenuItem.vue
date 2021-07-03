f42df479023d8a063577ad1d07a7f1146aabef52
Flora-Pvt <f.preuvot@live.fr>
2021-07-03 14:46:46 -0400

    MenuItem component

---
+++ cli-version/src/components/MenuItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="menu-item">
+    <img :src="image.source" :alt="image.alt" class="menu-item__image" />
+    <div>
+      <h3>{{ name }}</h3>
+      <p v-if="inStock">En stock</p>
+      <p v-else>En rupture de stock</p>
+      <div>
+        <label for="add-item-quantity">Quantité : {{ quantity }}</label>
+        <input id="add-item-quantity" type="number" v-model="quantity" />
+        <button @click="addToShoppingCart(quantity)">Ajouter au panier d'achat</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuItem",
+  props: ["addToShoppingCart", "image", "inStock", "name", "quantity"],
+};
+</script>
+
+<style scoped lang="scss"></style>
